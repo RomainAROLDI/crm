f0cba0d618fa99adac86714a17dc5f2f928da9d8
Baptiste Kenens <baptiste.kenens@gmail.com>
2023-01-17 16:17:06 +0100

    add update functionnality

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Customer;
+use App\Form\CustomerType;
 use App\Model\PaginatedDataModel;
 use App\Repository\CustomerRepository;
 use Knp\Component\Pager\PaginatorInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CustomerController extends AbstractController
 {
@@ -38,6 +40,25 @@ class CustomerController extends AbstractController
                 'Entreprise' => 'company',
                 'Créé par' => 'createdBy'
             ]))->getData()
+        ]);
+    }
+
+    #[Route('/client/edit/{id}', name: 'app_update_customer', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Customer $customer): Response
+    {
+        $form = $this->createForm(CustomerType::class, $customer);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('app_customer');
+        }
+
+        return $this->renderForm('customer/edit.html.twig', [
+            'customer' => $customer,
+            'form' => $form
         ]);
     }
 }
